906f1ff10b4c59203de5009954f8ecef042695d8
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-19 12:21:21 +0300

    mule

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,7 +11,7 @@ class FileStorage:
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         if cls == None:
-            return FileStorage.__objects
+            return self.__objects
         else:
             class_name = cls.__name__
             dct = {}
