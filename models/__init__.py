f2a8a3da1611f8246c956d1ec07cc8ff7a771e3b
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-04-19 16:47:13 +0300

    mule

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
+
 from os import getenv
+from os import getenv
+
 storage_type = getenv('HBNB_TYPE_STORAGE')
 
 if storage_type == 'db':
@@ -10,4 +12,5 @@ if storage_type == 'db':
 else:
     from models.engine.file_storage import FileStorage
     storage = FileStorage()
+
 storage.reload()
