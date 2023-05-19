02410a867385ad8ddbee085475ed2eb742dac496
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-05-19 11:28:31 +0300

    mule

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,20 +7,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     """Displays 'Hello HBNB!' from the root path"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Displays 'HBNB' from the root path"""
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
     """C is fun!"""
     return 'C {}'.format(text.replace('_', ' '))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
