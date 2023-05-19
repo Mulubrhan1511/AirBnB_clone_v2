026d8442115ec7281a0a0975f1ee9b2f8491a296
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-05-19 15:53:11 +0300

    ?mule

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
 """
-Starts a Flask web application that displays a list of states
+starts a Flask web application
 """
-from flask import Flask, render_template
-from models import storage
-from models.state import State
 
+from flask import Flask, render_template
+from models import *
+from models import storage
 app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State").values()
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Close the SQLAlchemy Session after each request."""
+    """closes the storage on teardown"""
     storage.close()
 
-
-@app.route('/cities_by_states', strict_slashes=False)
-def states_cities():
-    """Display a HTML page with a list of states and their cities."""
-    states = storage.all(State).values()
-    states_sorted = sorted(states, key=lambda s: s.name)
-
-    return render_template('8-cities_by_states.html', states=states_sorted)
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
