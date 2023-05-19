fac75aabd6666e7917865b7c83d936ee26bbe69e
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-05-19 11:31:56 +0300

    mule

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -7,20 +7,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     """Displays 'Hello HBNB!' from the root path"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Displays 'HBNB' from the root path"""
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
     """C is fun!"""
     return 'C {}'.format(text.replace('_', ' '))
+
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -28,10 +32,12 @@ def python(text):
     """Python is cool!"""
     return 'Python {}'.format(text.replace('_', ' '))
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """Is it a number?"""
     return '{} is a number'.format(n)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
