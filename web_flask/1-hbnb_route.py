1c4e147c0b8cce506af91a3332ead13bbe679897
Mulubrhan1511 <mulubrhangebrkidan@gmail.com>
2023-05-19 11:25:18 +0300

    mule

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,15 +7,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
     """Displays 'Hello HBNB!' from the root path"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Displays 'HBNB' from the root path"""
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
